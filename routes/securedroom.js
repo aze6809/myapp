68c9c820de1076c74ba5fb54b4769d057c6c3a4c
FullVolumeNoise <41074743+FullVolumeNoise@users.noreply.github.com>
2024-11-20 20:40:21 -0500

    semi added front end

--- routes/securedroom.js
+++ routes/securedroom.js
@@ -17,9 +17,15 @@ const isAuth = (req, res, next) => {
 };
 router.use(isAuth);
 
-router.get("/", (req, res) => {
+//display winning text without directing to HTML page
+router.get("/hero", (req, res) => {
   console.log("In room");
-  res.send("This is a new room");
+  res.send(`<p>You open the door. You're granted a magical sword. The hero continues their journey and eventually escapes.</p>`);
+});
+//display losing text without directing to HTML page
+router.get("/villain", (req, res) => {
+  console.log("In room");
+  res.send(`<p>You open the door. You're immediately hit with a wall of fire. The villain has been slain. Your journey is over.</p>`);
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -5,8 +5,23 @@ const room = require("./routes/securedroom");
 
 app.get("/", (req, res) => {
   console.log("Test");
-  res.send("Main Room");
+  res.send(`<p>You're in a maze and you've come to a dead end. There's only a door and a device to enter a password. 
+
+    It looks like you're supposed to enter your name and your role.</p>
+<form name="alignmentCheck" action="/submit" method="post">
+       <input type="radio" id="hero" name="hero" value="hero">
+        <label for="hero">Hero</label><br>
+        <input type="radio" id="villain" value="villain">
+        <label for="villain">Villain</label><br>
+        <input type="submit" value="Submit">
+    </form>`);
 });
+
+//need to go to auth page HERE
+app.post(`/submit`, (req, res) => {
+ // res.sendFile(__dirname + '/routes/securedroom.js');
+ //res.sendFile(__dirname + '/routes/views/hero_route.html');
+})
 
 app.use("/room", room);
 
