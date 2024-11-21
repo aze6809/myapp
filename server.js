2822d5c7ac879e9cf144ddad62c831d402382cfd
FullVolumeNoise <41074743+FullVolumeNoise@users.noreply.github.com>
2024-11-20 22:15:52 -0500

    added html files for routes and treasure room

--- server.js
+++ server.js
@@ -3,25 +3,20 @@ const app = express();
 
 const room = require("./routes/securedroom");
 
-app.get("/", (req, res) => {
+app.get("/", (req, res, next) => {
   console.log("Test");
-  res.send(`<p>You're in a maze and you've come to a dead end. There's only a door and a device to enter a password. 
-
-    It looks like you're supposed to enter your name and your role.</p>
-<form name="alignmentCheck" action="/submit" method="post">
-       <input type="radio" id="hero" name="hero" value="hero">
-        <label for="hero">Hero</label><br>
-        <input type="radio" id="villain" value="villain">
-        <label for="villain">Villain</label><br>
-        <input type="submit" value="Submit">
-    </form>`);
+  res.sendFile(__dirname + '/routes/views/index.html');
+ 
 });
 
 //need to go to auth page HERE
 app.post(`/submit`, (req, res) => {
- // res.sendFile(__dirname + '/routes/securedroom.js');
+  const data = req.body;
+  res.next();
+ console.log(data);
  //res.sendFile(__dirname + '/routes/views/hero_route.html');
 })
+
 
 app.use("/room", room);
 
